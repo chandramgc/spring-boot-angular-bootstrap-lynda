1efcd6b75a097e848a695161df3b80c6ed18e498
chandramgc1991 <chandramgc@gmail.com>
2017-09-16 21:47:51 -0700

    Spring boot push, get, pull and delete

---
+++ src/main/java/org/artha/springbootangularjs/SpringBootAngularjsLyndaApplication.java
@@ -0,0 +1,16 @@
+package org.artha.springbootangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+public class SpringBootAngularjsLyndaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularjsLyndaApplication.class, args);
+	}
+}
---
+++ src/main/java/org/artha/springbootangularjs/controller/ResourceConstants.java
@@ -0,0 +1,5 @@
+package org.artha.springbootangularjs.controller;
+
+public class ResourceConstants {
+	public static final String ROOM_RESERVATION_V1 = "/room/resvervation/v1";
+}
